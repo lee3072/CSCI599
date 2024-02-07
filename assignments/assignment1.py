8ca1f6e38f956f30c04a337c5e805b71254dfbd1
Seung Heon Lee <42813404+lee3072@users.noreply.github.com>
2024-02-06 18:20:52 -0800

    cpp

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -1,6 +1,27 @@
 import trimesh
 
-def subdivision_loop(mesh, iterations=1):
+def face_to_edge(face):
+    return [[face[i], face[(i+1)%3]] for i in range(face.size)]
+
+def subdivision_loop(mesh: trimesh.Trimesh, iterations=1):
+    # print(mesh.vertices)
+    # print(mesh.faces)
+    new_verticies = []
+    new_faces = []
+    # for each face 
+    for face in mesh.faces:
+        # create a list call edges
+        edges = face_to_edge(face)
+        vertices = [mesh.vertices[v] for v in face]
+        for edge in edges:
+            new_verticies.append((mesh.vertices[edge[0]] + mesh.vertices[edge[1]])/2)
+        # we need to replace the face with 4 new faces
+        # the first face is the original face
+        
+        print(edges)
+        print(vertices)            
+
+
     """
     Apply Loop subdivision to the input mesh for the specified number of iterations.
     :param mesh: input mesh
@@ -25,14 +46,14 @@ if __name__ == '__main__':
     print(f'Mesh Info: {mesh}')
 
     # apply loop subdivision over the loaded mesh
-    mesh_subdivided = mesh.subdivide_loop(iterations=1)
+    # mesh_subdivided = mesh.subdivide_loop(iterations=1)
     
     # TODO: implement your own loop subdivision here
-    # mesh_subdivided = subdivision_loop(mesh, iterations=1)
+    mesh_subdivided = subdivision_loop(mesh, iterations=1)
     
     # print the new mesh information and save the mesh
     print(f'Subdivided Mesh Info: {mesh_subdivided}')
-    mesh_subdivided.export('../assets/cube_subdivided.obj')
+    # mesh_subdivided.export('../assets/cube_subdivided.obj')
     
     # quadratic error mesh decimation
     mesh_decimated = mesh.simplify_quadric_decimation(8)
@@ -42,4 +63,4 @@ if __name__ == '__main__':
     
     # print the new mesh information and save the mesh
     print(f'Decimated Mesh Info: {mesh_decimated}')
-    mesh_decimated.export('../assets/cube_decimated.obj')
+    # mesh_decimated.export('../assets/cube_decimated.obj')
