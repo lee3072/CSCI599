1678a8ba3cd3fe1d31d7996d8d96881c77821667
Seung Heon Lee <42813404+lee3072@users.noreply.github.com>
2024-02-08 14:55:31 -0800

    Update assignment1.py

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -1,61 +1,45 @@
 import trimesh
+import numpy as np
 
 def face_to_edge(face):
     return [[face[i], face[(i+1)%3]] for i in range(face.size)]
 
 def subdivision_loop(mesh: trimesh.Trimesh, iterations=1):
-    vertices = mesh.vertices
-    faces = mesh.faces
-    edges = mesh.edges
-
-    print(edges)
-    # for each edge 
-    for edge in edges:
-        a = edge[0]
-        b = edge[1]
-        # find connected vertex from the edges that are connected to a and b
-        edges_a = [e for e in edges if a in e and b not in e]
-        edges_b = [e for e in edges if b in e and a not in e]
-        print(a, edges_a)
-        print(b, edges_b)
-        exit()
-        
-
-
-
-
-    # new_verticies = []
-    # new_faces = []
-    # # for each face 
-    # for face in mesh.faces:
-    #     # create a list call edges
-    #     edges = face_to_edge(face)
-    #     vertices = [mesh.vertices[v] for v in face]
-    #     for edge in edges:
-    #         new_verticies.append((mesh.vertices[edge[0]] + mesh.vertices[edge[1]])/2)
-    #     # we need to replace the face with 4 new faces
-    #     # the first face is the original face
-        
-    #     print(edges)
-    #     print(vertices)            
-
-
     """
     Apply Loop subdivision to the input mesh for the specified number of iterations.
     :param mesh: input mesh
     :param iterations: number of iterations
     :return: mesh after subdivision
     """
+    vertices = mesh.vertices.copy()
+    faces = mesh.faces
+    edges = mesh.edges
+
+    new_vertices = {}
+    # for each edge 
+    for edge in edges:
+        a, b = edge
+        # Find vertices connected to a but not b
+        a_adj = {e[1] for e in edges if e[0] == a and e[1] != b}.union({e[0] for e in edges if e[1] == a and e[0] != b})
+        # Find vertices connected to b but not a
+        b_adj = {e[1] for e in edges if e[0] == b and e[1] != a}.union({e[0] for e in edges if e[1] == b and e[0] != a})
+        # Find common vertices between a_adj and b_adj
+        common = list(a_adj.intersection(b_adj))
+        if len(common) == 2:
+            m = vertices[a] * 3/8 + vertices[b] * 3/8 + vertices[common[0]] * 1/8 + vertices[common[1]] * 1/8
+        else:
+            m = vertices[a] * 1/2 + vertices[b] * 1/2
+        print(a,b,m)
+        vertices = np.vstack([vertices, m])
+        if a < b:
+            new_vertices[(a,b)] = len(vertices) - 1
+        else:
+            new_vertices[(b,a)] = len(vertices) - 1
+    print(vertices)
+    print(new_vertices)
+    print(vertices[new_vertices[(6,7)]])
     return mesh
 
-def simplify_quadric_error(mesh, face_count=1):
-    """
-    Apply quadratic error mesh decimation to the input mesh until the target face count is reached.
-    :param mesh: input mesh
-    :param face_count: number of faces desired in the resulting mesh.
-    :return: mesh after decimation
-    """
-    return mesh
 
 if __name__ == '__main__':
     # Load mesh and print information
@@ -63,9 +47,6 @@ if __name__ == '__main__':
     mesh = trimesh.creation.box(extents=[1, 1, 1])
     print(f'Mesh Info: {mesh}')
 
-    # apply loop subdivision over the loaded mesh
-    # mesh_subdivided = mesh.subdivide_loop(iterations=1)
-    
     # TODO: implement your own loop subdivision here
     mesh_subdivided = subdivision_loop(mesh, iterations=1)
     
@@ -73,12 +54,3 @@ if __name__ == '__main__':
     print(f'Subdivided Mesh Info: {mesh_subdivided}')
     # mesh_subdivided.export('../assets/cube_subdivided.obj')
     
-    # quadratic error mesh decimation
-    # mesh_decimated = mesh.simplify_quadric_decimation(8)
-    
-    # TODO: implement your own quadratic error mesh decimation here
-    # mesh_decimated = simplify_quadric_error(mesh, face_count=1)
-    
-    # print the new mesh information and save the mesh
-    # print(f'Decimated Mesh Info: {mesh_decimated}')
-    # mesh_decimated.export('../assets/cube_decimated.obj')
