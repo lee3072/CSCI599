10fad07cbb22e5f426bace2ee74e39f8e809b749
Seung Heon Lee <42813404+lee3072@users.noreply.github.com>
2024-02-10 19:26:58 -0800

    Start Implementing Quadric Error

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -146,6 +146,14 @@ def subdivision_loop(mesh: trimesh.Trimesh, iterations=1):
     return mesh
 
 
+def simplify_quadric_error(mesh, face_count=1):
+    """
+    Apply quadratic error mesh decimation to the input mesh until the target face count is reached.
+    :param mesh: input mesh
+    :param face_count: number of faces desired in the resulting mesh.
+    :return: mesh after decimation
+    """
+    return mesh
 
 if __name__ == '__main__':
     # Load mesh and ic information
@@ -153,9 +161,16 @@ if __name__ == '__main__':
     mesh = trimesh.creation.box(extents=[1, 1, 1])
     ic(f'Mesh Info: {mesh}')
 
-    # TODO: implement your own loop subdivision here
-    mesh_subdivided = subdivision_loop(mesh, iterations=1)
+    # # TODO: implement your own loop subdivision here
+    # mesh_subdivided = subdivision_loop(mesh, iterations=1)
     
-    # ic the new mesh information and save the mesh
-    ic(f'Subdivided Mesh Info: {mesh_subdivided}')
-    mesh_subdivided.export('./assets/cube_subdivided.obj')
+    # # ic the new mesh information and save the mesh
+    # ic(f'Subdivided Mesh Info: {mesh_subdivided}')
+    # mesh_subdivided.export('./assets/cube_subdivided.obj')
+
+    # TODO: implement your own quadratic error mesh decimation here
+    mesh_decimated = simplify_quadric_error(mesh, face_count=1)
+    
+    # print the new mesh information and save the mesh
+    ic(f'Decimated Mesh Info: {mesh_decimated}')
+    mesh_decimated.export('./assets/cube_decimated.obj')
