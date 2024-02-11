9bc920b9009a7cbf97419d74d4ae0db4aca38437
Seung Heon Lee <42813404+lee3072@users.noreply.github.com>
2024-02-10 17:48:51 -0800

    Test Finished in a1 moved back to assignment1.py

--- assignments/assignment1.py
+++ assignments/assignment1.py
@@ -1,8 +1,9 @@
 import trimesh
 import numpy as np
 
-def get_new_vertex_index(new_vertices, a, b):
-    return new_vertices[(a,b) if a < b else (b,a)]
+def order_edge(a, b):
+    return (a, b) if a < b else (b, a)
+
 
 def subdivision_loop(mesh: trimesh.Trimesh, iterations=1):
     """
@@ -11,57 +12,134 @@ def subdivision_loop(mesh: trimesh.Trimesh, iterations=1):
     :param iterations: number of iterations
     :return: mesh after subdivision
     """
-    vertices = mesh.vertices.copy()
-    faces = mesh.faces.copy()
-    edges = mesh.edges.copy()
+    for i in range(iterations):
+        vertices = mesh.vertices.copy()
+        faces = mesh.faces.copy()
+        edges = mesh.edges.copy()
+        
+        v_e = {}    # Vertex to Connected Edges Dictionary
+        v_v = {}    # Vertex to Connected Vertices Dictionary
+        e_f = {}    # Edge to Connected Faces Dictionary
+        e_nv = {}   # Edge to New Vertex Dictionary
 
-    print(f'Vertices: {vertices.shape[0]}')
-    print(f'Faces: {faces.shape[0]}')
-    print(f'Edges: {edges.shape[0]}')
-    print(faces)
+        for e in range(edges.shape[0]):
+            edge = edges[e]
+            # Sort the edge to make sure the smaller vertex is first
+            edge = (edge[0], edge[1]) if edge[0] < edge[1] else (edge[1], edge[0])
+            edges[e] = edge
+            # Add the edge to the vertex to connected edge dictionary
+            v1, v2 = edge
+            v_e.setdefault(v1, set()).add(edge)
+            v_e.setdefault(v2, set()).add(edge)
+            # Add the vertex to the vertex to connected vertex dictionary
+            v_v.setdefault(v1, set()).add(v2)
+            v_v.setdefault(v2, set()).add(v1)
 
-    new_vertices = {}
-    # for each edge 
-    for edge in edges:
-        a, b = edge
-        # Find vertices connected to a
-        a_adj = {e[1] for e in edges if e[0] == a and e[1] != b}.union({e[0] for e in edges if e[1] == a and e[0] != b})
-        # Find vertices connected to b
-        b_adj = {e[1] for e in edges if e[0] == b and e[1] != a}.union({e[0] for e in edges if e[1] == b and e[0] != a})
-        # Find common vertices between a_adj and b_adj
-        common = list(a_adj.intersection(b_adj))
-        # if inboundary vertex, m = 3/4 * a + 1/8 * b + 1/8 * c
-        # if on boundary vertex, m = 1/2 * a + 1/2 * b
-        if len(common) == 2:
-            m = vertices[a] * 3/8 + vertices[b] * 3/8 + vertices[common[0]] * 1/8 + vertices[common[1]] * 1/8
-        else:
-            m = vertices[a] * 1/2 + vertices[b] * 1/2
-        # print(a,b,m)
-        vertices = np.vstack([vertices, m])
-        new_vertices[(a,b) if a < b else (b,a)] = len(vertices) - 1
+        print(f'# Vertices: {vertices.shape[0]}')
+        print(f'# Faces: {faces.shape[0]}')
+        print(f'# Edges: {edges.shape[0]}')
+        print(f'Vertex to Connected Edges:\n{v_e}\n')
+        print(f'Vertex to Connected Vertices:\n{v_v}\n')
 
-    # for each face
-    for f in range(faces.shape[0]):
-        face = faces[f].copy()
-        # create new faces with one even vertex and two odd vertices connected with it
-        # replace the old face with three odd vertices
-        for i in range(face.shape[0]+1):
-            a, b, c = face[i%3], face[(i+1)%3], face[(i+2)%3]
-            v1 = get_new_vertex_index(new_vertices, a, b)
-            v2 = get_new_vertex_index(new_vertices, a, c) if i == face.shape[0] else b
-            v3 = get_new_vertex_index(new_vertices, b, c)
-            if i == face.shape[0]: 
-                faces[f] = [v1, v2, v3]
-            else: 
-                faces = np.vstack([faces, [v1, v2, v3]])
-                    
-    # for even vertices find all connected vertices and calculate the new position
-    for i in range(mesh.vertices.shape[0]):
-        break
-    # print(vertices)
-    # print(new_vertices)
-    # print(vertices[new_vertices[(6,7)]])
+        for edge in edges:
+            a, b = edge
+            common = list(set(v_v[a]).intersection(set(v_v[b])))
+            if len(common) == 2:
+                m = vertices[a] * 3/8 + vertices[b] * 3/8 + vertices[common[0]] * 1/8 + vertices[common[1]] * 1/8
+            else:
+                m = vertices[a] * 1/2 + vertices[b] * 1/2
+            # m = vertices[a] * 1/2 + vertices[b] * 1/2
+            vertices = np.vstack([vertices, m])
+            # Add the new vertex to the edge to new vertex dictionary
+            e_nv[tuple(edge)] = len(vertices) - 1
+
+        print(f'Edge to New Vertices:\n{e_nv}\n')
+
+        # Establish connectivity with the new vertices
+        for f in range(mesh.faces.shape[0]):
+            face = faces[f].copy()
+            # create new faces with one even vertex and two odd vertices connected with it
+            # replace the old face with three odd vertices
+            for i in range(face.shape[0]+1):
+                a, b, c = face[i%3], face[(i+1)%3], face[(i+2)%3]
+                v1 = e_nv[order_edge(a, b)]
+                v2 = e_nv[order_edge(a, c)] if i == face.shape[0] else b
+                v3 = e_nv[order_edge(b, c)]
+                if i == face.shape[0]: 
+                    # replace the old face with three odd vertices
+                    nf = [v2, v1, v3]
+                    faces[f] = nf # Make Sure Normal is Correct
+                    # remove original vertices from the vertex to connected vertex dictionary
+                    if a in v_v:
+                        if b in v_v[a]: v_v[a].remove(b)
+                        if c in v_v[a]: v_v[a].remove(c)
+                    if b in v_v:
+                        if a in v_v[b]: v_v[b].remove(a)
+                        if c in v_v[b]: v_v[b].remove(c)
+                    if c in v_v:
+                        if a in v_v[c]: v_v[c].remove(a)
+                        if b in v_v[c]: v_v[c].remove(b)
+                    # remove original edges from the vertex to connected edge dictionary
+                    if a in v_e:
+                        if order_edge(a, b) in v_e[a]: v_e[a].remove(order_edge(a, b))
+                        if order_edge(a, c) in v_e[a]: v_e[a].remove(order_edge(a, c))
+                    if b in v_e:
+                        if order_edge(a, b) in v_e[b]: v_e[b].remove(order_edge(a, b))
+                        if order_edge(b, c) in v_e[b]: v_e[b].remove(order_edge(b, c))
+                    if c in v_e:
+                        if order_edge(a, c) in v_e[c]: v_e[c].remove(order_edge(a, c))
+                        if order_edge(b, c) in v_e[c]: v_e[c].remove(order_edge(b, c))
+                else: 
+                    # create new faces with one even vertex and two odd vertices connected with it
+                    nf = [v1, v2, v3]
+                    faces = np.vstack([faces, nf])
+                # add new edges to the vertex to connected vertex dictionary
+                v_v.setdefault(v1, set()).add(v2)
+                v_v.setdefault(v1, set()).add(v3)
+                v_v.setdefault(v2, set()).add(v1)
+                v_v.setdefault(v2, set()).add(v3)
+                v_v.setdefault(v3, set()).add(v1)
+                v_v.setdefault(v3, set()).add(v2)
+                # add new edges to the vertex to connected edge dictionary
+                v_e.setdefault(v1, set()).add(order_edge(v1, v2))
+                v_e.setdefault(v1, set()).add(order_edge(v1, v3))
+                v_e.setdefault(v2, set()).add(order_edge(v1, v2))
+                v_e.setdefault(v2, set()).add(order_edge(v2, v3))
+                v_e.setdefault(v3, set()).add(order_edge(v1, v3))
+                v_e.setdefault(v3, set()).add(order_edge(v2, v3))
+                e_f.setdefault(tuple(order_edge(v1, v2)), []).append(nf)
+                e_f.setdefault(tuple(order_edge(v1, v3)), []).append(nf)
+                e_f.setdefault(tuple(order_edge(v2, v3)), []).append(nf)
+
+
+        print(f'Updated Vertex to Connected Vertices:\n{v_v}\n')
+        print(f'Updated Vertex to Connected Edges:\n{v_e}\n')
+        print(f'Updated Faces:\n{faces}\n')
+        print(f'Edge to Connected Faces:\n{e_f}\n')
+
+        # for even vertices find all connected vertices and calculate the new position
+        for i in range(mesh.vertices.shape[0]):
+            v = vertices[i]
+            connected = v_v[i]
+            boundary = False
+            for c in connected:
+                if len(e_f[order_edge(i,c)]) == 1:
+                    boundary = True
+            if boundary:
+                v = 1/8 * (connected[0] + connected[1]) + 3/4 * v
+            else:
+                beta = 1/len(connected) * (5/8 - (3/8 + 1/4 * np.cos(2 * np.pi / len(connected)))**2)
+                v = v * (1-len(connected)*beta) + np.sum([vertices[j] for j in connected], axis=0) * beta
+            vertices[i] = v
+        print(f'Final Vertices:\n{vertices}\n')
+        print(f'Final Faces:\n{faces}\n')
+
+        # Create the new mesh
+        mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+
+
     return mesh
+
 
 
 if __name__ == '__main__':
@@ -75,5 +153,4 @@ if __name__ == '__main__':
     
     # print the new mesh information and save the mesh
     print(f'Subdivided Mesh Info: {mesh_subdivided}')
-    # mesh_subdivided.export('../assets/cube_subdivided.obj')
-    
+    mesh_subdivided.export('./assets/cube_subdivided.obj')
