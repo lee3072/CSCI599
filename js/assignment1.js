ea0a120c31d425e6eec2d910fc6843e102ad34e5
Seung Heon Lee <42813404+lee3072@users.noreply.github.com>
2024-02-13 10:59:49 -0800

    Update assignment1.js
    Both containers now display object

--- js/assignment1.js
+++ js/assignment1.js
@@ -11,6 +11,13 @@ let renderer, stats, gui;
 let scene, camera, controls, cube, dirlight, ambientLight;
 let isinitialized = false;
 
+const container2 = document.getElementById( 'container2' );
+container2.style.position = 'relative';
+
+let renderer2, stats2, gui2;
+let scene2, camera2, controls2, cube2, dirlight2, ambientLight2;
+let isinitialized2 = false;
+
 function initScene() {
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color( 0xffffff);
@@ -59,6 +66,56 @@ function initScene() {
 	camera.position.z = 5;
 }
 
+
+function initScene2() {
+	scene2 = new THREE.Scene();
+	scene2.background = new THREE.Color( 0xffffff);
+	camera2 = new THREE.PerspectiveCamera( 75, window.innerWidth / (window.innerHeight * 0.5), 0.1, 1000 );
+	
+	renderer2 = new THREE.WebGLRenderer();
+	renderer2.setSize( window.innerWidth, window.innerHeight * 0.5 );
+	container2.appendChild( renderer2.domElement );
+
+	controls2 = new OrbitControls( camera2, renderer2.domElement );
+	controls2.minDistance = 2;
+	controls2.maxDistance = 10;
+	controls2.addEventListener( 'change', function() { renderer2.render( scene2, camera2 ); });
+
+	dirlight2 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+	dirlight2.position.set( 0, 0, 1 );
+	scene2.add( dirlight2 );
+
+	ambientLight2 = new THREE.AmbientLight( 0x404040, 2 );
+	scene2.add( ambientLight2 );
+
+
+	// the loading of the object is asynchronous
+	let loader = new OBJLoader();
+	loader.load( 
+		// resource URL
+		'../assets/cube_decimated.obj', 
+		// called when resource is loaded
+		function ( object ) {
+			cube2 = object.children[0];
+			cube2.material = new THREE.MeshPhongMaterial( { color: 0x999999 });
+			cube2.position.set( 0, 0, 0 );
+			cube2.name = "cube2";
+			scene2.add( cube2 );
+		},
+		// called when loading is in progresses
+		function ( xhr ) {
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+		},
+		// called when loading has errors
+		function ( error ) {
+			console.log( 'An error happened' + error);
+		}
+	);
+	
+	camera2.position.z = 5;
+}
+
+
 function initSTATS() {
 	stats = new Stats();
 	stats.showPanel( 0 );
@@ -66,6 +123,13 @@ function initSTATS() {
 	stats.dom.style.top = 0;
 	stats.dom.style.left = 0;
 	container.appendChild( stats.dom );
+
+	stats2 = new Stats();
+	stats2.showPanel( 1 );
+	stats2.dom.style.position = 'absolute';
+	stats2.dom.style.top = 0;
+	stats2.dom.style.left = 0;
+	container2.appendChild( stats2.dom );
 }
 
 function initGUI() {
@@ -81,6 +145,18 @@ function initGUI() {
 		container.appendChild( gui.domElement );
 		isinitialized = true;
 	}
+	if (!isinitialized2) {
+		gui2 = new GUI();
+		cube2 = scene2.getObjectByName( "cube2" );
+		gui2.add( cube2.position, 'x', -1, 1 );
+		gui2.add( cube2.position, 'y', -1, 1 );
+		gui2.add( cube2.position, 'z', -1, 1 );
+		gui2.domElement.style.position = 'absolute';
+		gui2.domElement.style.top = '0px';
+		gui2.domElement.style.right = '0px';
+		container2.appendChild( gui2.domElement );
+		isinitialized2 = true;
+	}
 }
 
 function animate() {
@@ -95,16 +171,30 @@ function animate() {
 
 	renderer.render( scene, camera );
 	stats.update();
+
+	cube2 = scene2.getObjectByName( "cube2" );
+	if (cube2) {
+		cube2.rotation.x += 0.01;
+		cube2.rotation.y += 0.01;
+		initGUI(); // initialize the GUI after the object is loaded
+	}
+
+	renderer2.render( scene2, camera2 );
+	stats2.update();
 }
 
 function onWindowResize() {
 	camera.aspect = window.innerWidth / (window.innerHeight * 0.5);
 	camera.updateProjectionMatrix();
 	renderer.setSize( window.innerWidth, window.innerHeight * 0.5 );
+	camera2.aspect = window.innerWidth / (window.innerHeight * 0.5);
+	camera2.updateProjectionMatrix();
+	renderer2.setSize( window.innerWidth, window.innerHeight * 0.5 );
 };
 
 window.addEventListener( 'resize', onWindowResize, false );
 
 initScene();
+initScene2()
 initSTATS();
 animate();
